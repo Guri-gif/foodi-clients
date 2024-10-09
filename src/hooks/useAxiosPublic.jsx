6c643280234b72431b528fefe9f418909f6e5cb9
Guri-gif <gursewxk69@gmail.com>
2024-10-10 00:26:21 +0530

    Update useAxiosPublic.jsx

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'foodi-server-2.onrender.com'
 })
 
 const useAxiosPublic = () => {
